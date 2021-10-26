4cd1b38bb6243f6fa023f9881e5a379569fd5925
Rex <yuzhedai430@gmail.com>
2021-10-26 10:48:59 +1000

    minor style changes

--- src/Events/PoIThumbnail.js
+++ src/Events/PoIThumbnail.js
@@ -1,26 +1,33 @@
 import * as THREE from 'three'
 import img from '../Images/warPic.png'
-import React, { useContext, useMemo} from "react";
+import React, {useContext, useMemo} from "react";
 import tweenCamera from "./CameraTravese";
-import { EventContext, TimelineState } from "./EventContext";
-import { useThree } from "@react-three/fiber";
+import {EventContext, TimelineState} from "./EventContext";
+import {useThree} from "@react-three/fiber";
 import PoIAlbum from "./PoIAlbum";
 import {TextureLoader} from "three";
 
 
 export const PoIThumbnail = (props) => {
-    const textureImg = require("../Images/POI-thumbnails/" + props.event.category +"/" + props.event.thumbnail)
-    const { eventState, setEventState, timelinePos, setTimelinePos, setActivePoster, cameraOffset } = useContext(EventContext)
+    const textureImg = require("../Images/POI-thumbnails/" + props.event.category + "/" + props.event.thumbnail)
+    const {
+        eventState,
+        setEventState,
+        timelinePos,
+        setTimelinePos,
+        setActivePoster,
+        cameraOffset
+    } = useContext(EventContext)
 
 
-    const { camera } = useThree();
+    const {camera} = useThree();
     // offset from poster
     // const cameraOffset = [0,-5.5,0]
 
     function handleClick() {
         const curPosition = new THREE.Vector3().copy(camera.position);
         // same as poimarker
-        if(eventState === TimelineState.TIMELINE){
+        if (eventState === TimelineState.TIMELINE) {
             setEventState(TimelineState.DISABLED)
             console.log(curPosition)
             setTimelinePos(curPosition)
@@ -32,16 +39,16 @@ export const PoIThumbnail = (props) => {
         }
     }
 
-    function handleHoverIn(){
-        
-        if(eventState === TimelineState.TIMELINE){
+    function handleHoverIn() {
+
+        if (eventState === TimelineState.TIMELINE) {
             props.hoverIn(props.index)
         }
     }
 
-    function handleHoverOut(){
+    function handleHoverOut() {
 
-        if(eventState === TimelineState.TIMELINE){
+        if (eventState === TimelineState.TIMELINE) {
             props.hoverOut()
         }
     }
@@ -53,19 +60,19 @@ export const PoIThumbnail = (props) => {
     return (
 
         <group rotation={props.rotation}
-            position={props.position}
-            onClick={handleClick}
-            onPointerEnter={handleHoverIn}
-            onPointerLeave={handleHoverOut}
+               position={props.position}
+               onClick={handleClick}
+               onPointerEnter={handleHoverIn}
+               onPointerLeave={handleHoverOut}
         >
             <mesh>
-                <planeGeometry args={[9, 9]} />
-                <meshBasicMaterial map={texture} />
+                <planeGeometry args={[9, 9]}/>
+                <meshBasicMaterial map={texture}/>
             </mesh>
-            {/* <mesh position={[0,0,-0.3]}>
-                <boxGeometry args={[12.9,10.8,0.5]}/>
+            <mesh position={[0, 0, -0.3]}>
+                <boxGeometry args={[12.9, 10.8, 0.5]}/>
                 <meshBasicMaterial map={props.brickTexture}/>
-            </mesh> */}
+            </mesh>
         </group>
     )
 }
